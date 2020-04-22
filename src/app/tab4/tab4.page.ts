29e9566a1df103bb349cb984324d2dfa3f8a1d87
Hectro <romainhector@yahoo.fr>
2020-04-22 16:29:02 +0200

    temperature2

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -27,48 +27,49 @@ export class Tab4Page implements OnInit {
 
   
   convTemp(){
+    if(this.tempSaisi){
 
-    if(this.unite == 'celsius' && this.uniteConv == 'fahrenC'){
-      console.log('celsius to fahren')
-      this.resultatC = this.tempSaisi*1.8 + 32;
-      this.reuls = Math.round(this.resultatC * 100) / 100
-    }
+      if(this.unite == 'celsius' && this.uniteConv == 'fahrenC'){
+        console.log('celsius to fahren')
+        this.resultatC = this.tempSaisi*1.8 + 32;
+        this.reuls = Math.round(this.resultatC * 100) / 100
+      }
 
-    if(this.unite == 'celsius' && this.uniteConv == 'kelvinC'){
-      console.log('celsius to kelvin')
-      this.resultatC = this.tempSaisi*1 + 273.15;
-      this.reuls = Math.round(this.resultatC * 100) / 100
+      if(this.unite == 'celsius' && this.uniteConv == 'kelvinC'){
+        console.log('celsius to kelvin')
+        this.resultatC = this.tempSaisi*1 + 273.15;
+        this.reuls = Math.round(this.resultatC * 100) / 100
 
-    }
+      }
 
-    if(this.unite == 'kelvin' && this.uniteConv == 'celciusC'){
-      console.log('kelvin to celcius')
-      this.resultatC = this.tempSaisi*1 - 273.15;
-      this.reuls = Math.round(this.resultatC * 100) / 100
+      if(this.unite == 'kelvin' && this.uniteConv == 'celciusC'){
+        console.log('kelvin to celcius')
+        this.resultatC = this.tempSaisi*1 - 273.15;
+        this.reuls = Math.round(this.resultatC * 100) / 100
 
-    }
+      }
 
-    if(this.unite == 'kelvin' && this.uniteConv == 'fahrenC'){
-      console.log('kelvin to fahren')
-      this.resultatC = (this.tempSaisi*1 - 273.15) * 1.8 + 32;
-      this.reuls = Math.round(this.resultatC * 100) / 100
+      if(this.unite == 'kelvin' && this.uniteConv == 'fahrenC'){
+        console.log('kelvin to fahren')
+        this.resultatC = (this.tempSaisi*1 - 273.15) * 1.8 + 32;
+        this.reuls = Math.round(this.resultatC * 100) / 100
 
-    }
+      }
 
-    if(this.unite == 'fahren' && this.uniteConv == 'celciusC'){
-      console.log('farhren to celsius')
-      this.resultatC = (this.tempSaisi*1 - 32 ) / 1.8;
-      this.reuls = Math.round(this.resultatC * 100) / 100
+      if(this.unite == 'fahren' && this.uniteConv == 'celciusC'){
+        console.log('farhren to celsius')
+        this.resultatC = (this.tempSaisi*1 - 32 ) / 1.8;
+        this.reuls = Math.round(this.resultatC * 100) / 100
 
-    }
+      }
 
-    if(this.unite == 'fahren' && this.uniteConv == 'kelvinC'){
-      console.log('fahren to kelvin')
-      this.resultatC = (this.tempSaisi*1 - 32 ) / 1.8 + 273.15;
-      this.reuls = Math.round(this.resultatC * 100) / 100
+      if(this.unite == 'fahren' && this.uniteConv == 'kelvinC'){
+        console.log('fahren to kelvin')
+        this.resultatC = (this.tempSaisi*1 - 32 ) / 1.8 + 273.15;
+        this.reuls = Math.round(this.resultatC * 100) / 100
 
+      }
     }
   }
-
 
 }
